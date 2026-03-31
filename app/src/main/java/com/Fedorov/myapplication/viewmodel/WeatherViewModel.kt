579d4c3f5446ea31075d50ae854c0ed5423527f0
duxa4854 <nzaichenko531@gmail.com>
2026-03-31 13:46:57 +0300

    Step 8: Implement parallel loading with async

--- app/src/main/java/com/Fedorov/myapplication/viewmodel/WeatherViewModel.kt
+++ app/src/main/java/com/Fedorov/myapplication/viewmodel/WeatherViewModel.kt
@@ -8,14 +8,18 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.async
+import java.lang.Exception
 
 class WeatherViewModel: ViewModel() {
     private val repository = WeatherRepository()
     private val _weatherState = MutableStateFlow(WeatherData())
-    val weatherState:StateFlow<WeatherData> = _weatherState.asStateFlow()
+    val weatherState: StateFlow<WeatherData> = _weatherState.asStateFlow()
+
     init {
         loadWeatherData()
     }
+
     fun loadWeatherData() {
         viewModelScope.launch {
             _weatherState.value = _weatherState.value.copy(
@@ -23,19 +27,36 @@ class WeatherViewModel: ViewModel() {
                 error = null
             )
             try {
-
-                val temperature = repository.fetchTemperature()
-                _weatherState.value = _weatherState.value.copy(temperature = temperature)
-                val humidity = repository.fetchTemperature()
-                _weatherState.value = _weatherState.value.copy(humidity = humidity)
-                val windSpeed = repository.fetchTemperature()
-                _weatherState.value = _weatherState.value.copy(windSpeed = windSpeed)
-                _weatherState.value = _weatherState.value.copy(isLoading = false)
-            } catch (e: Exception) {
-                _weatherState.value = _weatherState.value.copy(
+                val temperatureDeferred = async { repository.fetchTemperature() }
+                val humidityDeferred = async { repository.fetchHumidity() }
+                val windSpeedDeferred = async { repository.fetchWindSpeed() }
+                val temperature = temperatureDeferred.await()
+                val humidity = humidityDeferred.await()
+                val windSpeed = windSpeedDeferred.await()
+                _weatherState.value = WeatherData(
+                    temperature = temperature,
+                    humidity = humidity,
+                    windSpeed = windSpeed,
                     isLoading = false,
-                    error = "Ошибка загрузки: ${e.message}"
+                    error = null
                 )
+            } catch (e: Exception) {
+
+//                val temperature = repository.fetchTemperature()
+//                _weatherState.value = _weatherState.value.copy(temperature = temperature)
+//                val humidity = repository.fetchTemperature()
+//                _weatherState.value = _weatherState.value.copy(humidity = humidity)
+//                val windSpeed = repository.fetchTemperature()
+//                _weatherState.value = _weatherState.value.copy(windSpeed = windSpeed)
+//                _weatherState.value = _weatherState.value.copy(isLoading = false)
+//            } catch (e: Exception) {
+//                _weatherState.value = _weatherState.value.copy(
+//                    isLoading = false,
+//                    error = "Ошибка загрузки: ${e.message}"
+//                )
+//            }
+//        }
+//    }
             }
         }
     }
